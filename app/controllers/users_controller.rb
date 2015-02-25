9eb1e84d05c0d2339daf8f3248362347c47fa358
Trey Jackson <jacksrf@gmail.com>
2015-02-25 17:44:48 -0600

    new database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,21 @@ def new
   @user = User.new
 end
 
+def create
+    # Create the user from params
+    @user = User.new(params[:user])
+    if @user.save
+      # Deliver the signup email
+      UserNotifier.send_signup_email(@user).deliver
+      redirect_to '/'
+    else
+      render :action => 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:naem, :email, :message)
+    end
+
 end
---
+++ app/mailers/user_notifier.rb
@@ -0,0 +1,10 @@
+class UserNotifier < ActionMailer::Base
+  default :from => @user.email
+
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_signup_email(user)
+    @user = user
+    mail( :to => "jacksrf@gmail.com",
+    :subject => 'Thanks for signing up for our amazing app' )
+  end
+end
