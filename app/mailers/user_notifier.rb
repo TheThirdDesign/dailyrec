d99d12bd5f700ab34ab8555ba1c4153711af7504
Trey Jackson <jacksrf@gmail.com>
2015-02-25 18:17:51 -0600

    new database

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,10 +1,10 @@
 class UserNotifier < ActionMailer::Base
-  default :from => "<%= @user.email %>"
+  default :from => 'Contact@dailyrec.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
     @user = user
     mail( :to => "jacksrf@gmail.com",
-    :subject => 'Thanks for signing up for our amazing app' )
+    :subject => 'Somone just contacted you' )
   end
 end
