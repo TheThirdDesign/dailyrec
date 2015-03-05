70372bfbf42e328a4e5ff290f2750fb23b4c3fd1
Trey Jackson <jacksrf@gmail.com>
2015-03-05 16:31:30 -0600

    new home page updates

--- app/assets/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("li .nonlogo").mouseover(function(){
-   $("li p").stop().slideDown("slow");
-  });
-  $("li .nonlogo").mouseout(function(){
-   $("li p").slideUp("slow");
-  });
- });
