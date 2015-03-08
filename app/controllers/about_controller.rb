f925a4028bb5e046509df05faf0fce0f79351503
Trey Jackson <jacksrf@gmail.com>
2015-03-08 17:17:14 -0500

    final update for phase 1

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -3,9 +3,13 @@ class AboutController < ApplicationController
     response = HTTParty.get('https://api.instagram.com/v1/users/self/media/recent/?access_token=1678934706.7497432.f3048715df0940b69ed625db492c45f5')
     @response = response["data"]
     @images = []
+    @counter = 1
     @response.each do |image|
+      if @counter <= 5
       imageURL = image["images"]["standard_resolution"]["url"]
       @images.push(imageURL)
     end
+    @counter += 1
+  end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,17 @@ class UsersController < ApplicationController
 
 def new
   @user = User.new
+  response = HTTParty.get('https://api.instagram.com/v1/users/self/media/recent/?access_token=1678934706.7497432.f3048715df0940b69ed625db492c45f5')
+  @response = response["data"]
+  @images = []
+  @counter = 1
+  @response.each do |image|
+    if @counter <= 5
+    imageURL = image["images"]["standard_resolution"]["url"]
+    @images.push(imageURL)
+  end
+  @counter += 1
+end
 end
 
 def create
