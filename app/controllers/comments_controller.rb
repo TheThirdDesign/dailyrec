6d0da8304878a61daef97a8db36ee684cb099935
Trey Jackson <jacksrf@gmail.com>
2015-03-13 10:40:25 -0500

    update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,12 @@ class CommentsController < ApplicationController
 def create
   @comment = Comment.create(comment_params)
   @comments = Comment.where(post_id: params[:id])
-  redirect_to post_path(params[:id])
+  redirect_to post_path(params[:post_id])
 end
 
 def edit
 @comment = Comment.find_by(id: params[:id])
+
 if @comment.likes == nil
   @comment.likes = 1
   @comment.save
