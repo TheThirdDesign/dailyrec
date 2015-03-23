315eb6243a657464a090d55a4bfd2a8a29a3ef13
Trey Jackson <jacksrf@gmail.com>
2015-03-23 14:56:21 -0500

    fixed display issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,5 +40,4 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.assets.precompile += %w( style.css.scss )
 
-  Paperclip.options[:command_path] = "/usr/local/bin/convert"
 end
