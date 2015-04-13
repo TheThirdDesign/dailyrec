48bf2e9198f91eeb8e24570f6d7b9c273ba33a49
Trey Jackson <jacksrf@gmail.com>
2015-04-13 11:26:32 -0500

    added text editor

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
+
   :domain => 'dailyrec.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
